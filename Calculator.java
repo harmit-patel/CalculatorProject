8851387d91645dc166da192887f0450e901c2f86
harmit <pharmit2644@gmail.com>
2025-11-26 09:07:54 +0530

    Added Calculator program (add & subtract)

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + sub);
+        sc.close();
+    }
+}
