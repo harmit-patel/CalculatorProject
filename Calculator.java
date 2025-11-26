49dd67d4c9d431c6f30115f24c92443935134198
harmit <pharmit2644@gmail.com>
2025-11-26 09:16:14 +0530

    Added multiplication and division  program

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Calculator {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int num1 = sc.nextInt();
@@ -9,8 +11,15 @@ public class Calculator {
         int num2 = sc.nextInt();
         int sum = num1 + num2;
         int sub = num1 - num2;
+        int mul = num1 * num2;
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        if (num2 != 0) {
+            System.out.println("Division: " + div);
+        } else {
+            System.out.println("Division: Cannot divide by zero");
+        }
         sc.close();
     }
 }
